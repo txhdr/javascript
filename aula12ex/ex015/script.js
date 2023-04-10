7833b6d1ac071be06d2b97a07bfc838941f4a926
txhdr <109394306+txhdr@users.noreply.github.com>
2023-04-09 22:36:15 -0300

    update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -5,9 +5,40 @@ function verificar(){
     var res = document.querySelector('div#res')
 
     if (fano.value.length == 0 || fano.value > ano){
-        window.alert('[ERRRO]verifique os dados e tente novamente')
+        window.alert('[ERRO]verifique os dados e tente novamente')
     } else{
-        window.alert('tudo ok')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade Calculada: ${idade}`
+        var gênero = ''
+        var img =  document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked){
+            gênero = 'Homem'
+            if (idade>= 0 && idade > 10){
+                //criança
+            } else if (idade < 21){
+                //jovem
+            } else if (idade < 50){
+                //adulto
+            } else {
+                //idoso
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade>= 0 && idade > 10){
+                //criança
+            } else if (idade < 21){
+                //jovem
+            } else if (idade < 50){
+                //adulto
+            } else {
+                //idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
 
 }
+
