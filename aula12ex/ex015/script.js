b52fdbe27e31506570b76c5521910f96b1c56181
txhdr <109394306+txhdr@users.noreply.github.com>
2023-04-10 21:12:47 -0300

    update de aula

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,43 +1,60 @@
+
 function verificar(){
+    
     var data = new Date()
     var ano = data.getFullYear()
+
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+
 
     if (fano.value.length == 0 || fano.value > ano){
         window.alert('[ERRO]verifique os dados e tente novamente')
     } else{
+        
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade Calculada: ${idade}`
+
         var gênero = ''
         var img =  document.createElement('img')
         img.setAttribute('id','foto')
         if (fsex[0].checked){
             gênero = 'Homem'
-            if (idade>= 0 && idade > 10){
+            if (idade>= 0 && idade < 10){
                 //criança
+                img.setAttribute('src', 'foto-bebe-m.png')
             } else if (idade < 21){
                 //jovem
+                img.setAttribute('src', 'menino.png')
             } else if (idade < 50){
                 //adulto
+                img.setAttribute('src', 'homem.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'idoso.png')
             }
+
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
-            if (idade>= 0 && idade > 10){
+            if (idade>= 0 && idade < 10){
                 //criança
+                img.setAttribute('src', 'criança-xx.png')
             } else if (idade < 21){
                 //jovem
+                img.setAttribute('src', 'menina.png')
             } else if (idade < 50){
                 //adulto
+                img.setAttribute('src', 'mulher.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'idosa.png')
             }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    res.appendChild(img)
     }
 
 }
---
+++ praticando/aula-12-treino/script.js
@@ -0,0 +1,12 @@
+function verificador(){
+
+    data = new Date()
+    ano = data.getFullYear()
+
+    var fano = window.document.getElementById('txtano').value
+    
+    
+    //window.alert(dataAtual)
+    
+
+}
