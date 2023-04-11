fd57a1dc528d230d0d3b1ee241edff805df66f9c
txhdr <109394306+txhdr@users.noreply.github.com>
2023-04-10 21:24:41 -0300

    praticando exercicios
    praticando o exercicio de data e de colocar foto no código.

--- praticando/aula-12-treino/script.js
+++ praticando/aula-12-treino/script.js
@@ -3,10 +3,18 @@ function verificador(){
     data = new Date()
     ano = data.getFullYear()
 
-    var fano = window.document.getElementById('txtano').value
+    var fano = window.document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano ) {
+        window.alert('[ERRO] verifique os dados e tente novamente')
+    } else { 
+        var fsex = document.getElementById('sex')
+        var idade = ano - Number(fano.value)
+    } 
     
     
-    //window.alert(dataAtual)
     
 
 }
+//window.alert('tudo certo')
